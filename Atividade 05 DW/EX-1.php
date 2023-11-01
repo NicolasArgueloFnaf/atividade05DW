e181398cfcd7a75657fa7d673410d2f6b8d559f9
NicolasArgueloFnaf <141849216+NicolasArgueloFnaf@users.noreply.github.com>
2023-11-01 11:30:16 -0400

    Add files via upload

---
+++ Atividade 05 DW/EX-1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initialscale=1.0">
+    <title>Chaves</title>
+</head>
+
+<body>
+
+    <h1>
+        <?php 
+        echo "Dragon Ball\u{1F432}";
+        ?>
+    </h1>
+
+    <p>Kiko</p>
+
+</body>
+
+</html>
